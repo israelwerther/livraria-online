c0a7a33ac8195eb72d8cf8f31d298ee3378e8d95
israelwerther <israelwerther48@outlook.com>
2024-09-10 12:58:58 -0300

    remove codigo desnecessário

--- livrariaonline/core/urls.py
+++ livrariaonline/core/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from livrariaonline.core.views import Index
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', Index.as_view(), name='index'),
+    
 ]
--- livrariaonline/core/views.py
+++ livrariaonline/core/views.py
@@ -1,10 +1,2 @@
-from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
 
-
-class Index(TemplateView):
-    template_name = 'core/index.html'
     
-    def test_func(self):
-        return self.request.user.is_superuser
