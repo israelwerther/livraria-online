ee61f3fe2e9e5c0860e6b7a2b37b5a6b802218c9
israelwerther <israelwerther48@outlook.com>
2024-09-11 17:56:48 -0300

    adiciona serializer

--- livrariaonline/books/models.py
+++ livrariaonline/books/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import User
 
 class Book(models.Model):
     title = models.CharField(max_length=255, null=True, blank=True)
     author = models.CharField(max_length=255, null=True, blank=True)
     first_publish_year = models.IntegerField(null=True, blank=True)
     cover_id = models.IntegerField(null=True, blank=True)
+    price = models.DecimalField("Preço", max_digits=8, decimal_places=2, default=100)
     open_library_id = models.CharField(max_length=50, unique=True, null=True, blank=True)
     logged_date = models.DateTimeField(null=True, blank=True)
 
@@ -21,12 +21,41 @@ class Book(models.Model):
 
 
 class Cart(models.Model):
-    session_key = models.CharField(max_length=40, unique=True)
+    user = models.OneToOneField("accounts.User", verbose_name=("Usuário"), on_delete=models.CASCADE, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Carrinho - {self.created_at}"
+    
+    def add_book(self, book_id, quantity):
+        CartItem.objects.update_or_create(
+            cart=self,
+            book=Book.objects.get(id=book_id),
+            defaults={
+                "quantity": quantity
+            }
+        )
 
+    def clean_cart(self):
+        self.items.all().delete()
+
+    @property
+    def total(self):
+        return self.items.aggregate(total=models.Sum('book__price')).get('total') or 0
+    
+    def finalizar_compra(self, payment_type):
+        order = Order.objects.create(
+            user=self.user
+        )
+        
+        for item in self.items.all():
+            order_item = OrderItem.objects.create(
+                order=order,
+                book=item.book,
+                quantity=item.quantity,
+            )
+        
+        self.clean_cart()
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
@@ -38,7 +67,7 @@ class CartItem(models.Model):
     
 
 class Order(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='Orders', null=True, blank=True)
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='orders', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- livrariaonline/books/serializers.py
+++ livrariaonline/books/serializers.py
@@ -1,23 +1,30 @@
 from rest_framework import serializers
 from .models import Cart, CartItem
-from .models import Book  # Importar o modelo Book
+from .models import Book
 
-# Serializer para os itens do carrinho
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'author', 'price', 'cover_url']
+
 class CartItemSerializer(serializers.ModelSerializer):
-    book_title = serializers.CharField(source='book.title', read_only=True)
-    book_author = serializers.CharField(source='book.author', read_only=True)
-    book_price = serializers.DecimalField(source='book.price', max_digits=10, decimal_places=2, read_only=True)
-    book_cover_url = serializers.CharField(source='book.cover_url', read_only=True)
+    title = serializers.CharField(source='book.title', read_only=True)
+    author = serializers.CharField(source='book.author', read_only=True)
+    price = serializers.DecimalField(source='book.price', max_digits=10, decimal_places=2, read_only=True)
+    cover_url = serializers.CharField(source='book.cover_url', read_only=True)
+    id = serializers.SerializerMethodField()
 
     class Meta:
         model = CartItem
-        fields = ['book', 'book_title', 'book_author', 'book_price', 'book_cover_url', 'quantity']
+        fields = ['id', 'title', 'author', 'price', 'cover_url', 'quantity']
 
-# Serializer para o carrinho
+    def get_id(self, obj):
+        return obj.book.id
+
 class CartSerializer(serializers.ModelSerializer):
     items = CartItemSerializer(many=True, read_only=True)
     total_items = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Cart
-        fields = ['id', 'user', 'created_at', 'items', 'total_items']
+        fields = ['id', 'items', 'total_items']
