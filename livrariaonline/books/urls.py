2a47e516fcfa71d4294bd8a0193b22ebc10179ac
israelwerther <israelwerther48@outlook.com>
2024-09-11 17:57:37 -0300

    ajusta rotas para api

--- livrariaonline/books/urls.py
+++ livrariaonline/books/urls.py
@@ -1,16 +1,22 @@
 from django.urls import path, include
 from . import views
+from .api import CartViewSet
+from rest_framework.routers import DefaultRouter
 
-# router = DefaultRouter()
-# router.register(r'carts', CartViewSet, basename='cart')
+router = DefaultRouter()
+router.register(r'carts', CartViewSet, basename='carts')
+
+app_name = 'books'
 
 urlpatterns = [
     path('', views.MyBooksListView.as_view(), name='books_list'),
     path('cart/', views.CartListView.as_view(), name='cart'),
     path('cart/remove/', views.CartItemRemoveView.as_view(), name='cart_remove'),
-    path('checkout/', views.CheckoutView.as_view(), name='checkout'),
-    path('orders/', views.MyOrdersListView.as_view(), name='my_orders'),  # URL para a página de compras realizadas
+    path('checkout/', views.CheckoutTemplateView.as_view(), name='checkout'),
+    path('orders/', views.MyOrdersListView.as_view(), name='my_orders'),
     path('order/complete/<int:order_id>/', views.OrderSuccessView.as_view(), name='order_success'),
     path('order/success-redirect/', views.OrderSuccessRedirectView.as_view(), name='order_success_redirect'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
-]
+] 
+
+urlpatterns += router.urls
